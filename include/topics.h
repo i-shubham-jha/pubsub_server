89676587933267cba7cec345b00378b67fdfa02a
shubham <>
2024-06-11 18:48:17 +0530

    working topics with fs

--- include/topics.h
+++ include/topics.h
@@ -1,21 +1,34 @@
 // Topics is a separate class with helper functions
 #include <string>
-#include <unordered_set>
+#include <unordered_map>
 
+// encapsulation for the topics list ds and corresponding functions.
+// This class maintains a map from the topic name to the number of
+// messages currently there in the system for that topic.
+// See cpp file for description of functions
 class Topics
 {
-    std::unordered_set<std::string> topics_list;
+    std::unordered_map<std::string, unsigned> topics;
     std::string topics_directory;
 
   public:
     Topics();
 
     // checks if a topic there or not
-    bool check_topic(std::string const topic);
+    inline bool exists(std::string const topic);
 
     // create a new topic
     void create_topic(std::string const topic);
 
     // delete topic
-    void delete_topic(std::string const topic);
+    void delete_topic(std::string const topic, bool delete_directory);
+
+    // increment message count
+    void increment_mssg_cnt(std::string const topic);
+
+    // decrement message count
+    void decrement_mssg_cnt(std::string const topic);
+
+    // get message count
+    unsigned get_mssg_cnt(std::string const topic);
 };
--- src/topics.cpp
+++ src/topics.cpp
@@ -1,13 +1,13 @@
+#include "topics.h"
 #include "logger.h"
 #include <filesystem>
-#include <topics.h>
 
 namespace fs = std::filesystem;
 
-// constructor
+// Defautl constructor
 // inits the topics_directory var with the directory where
-// topics and mssgs should be stored
-// also creates the holding directory for topics and mssgs
+// topics and mssgs should be stored.
+// Also creates the holding directory for topics and mssgs on hdd.
 Topics::Topics()
 {
     this->topics_directory = "./messages";
@@ -21,16 +21,22 @@ Topics::Topics()
     INFO_LOG("Topics object init done");
 }
 
-bool Topics::check_topic(std::string const topic)
+// function to find whether the given topic exists in the ds or not
+inline bool Topics::exists(std::string const topic)
 {
-    return topics_list.find(topic) != topics_list.end();
+    return topics.find(topic) != topics.end();
 }
 
+// function to create a new topic
+// also creates the directory for this new topic
 void Topics::create_topic(std::string const topic)
 {
-    if (!check_topic(topic))
+    if (!exists(topic))
     {
-        topics_list.insert(topic);
+        // inserting into map
+        topics.insert({topic, 0});
+
+        // creating directory for this topic
         fs::path path = (topics_directory + "/" + topic);
         if (!fs::exists(path))
         {
@@ -40,6 +46,7 @@ void Topics::create_topic(std::string const topic)
         {
             WARN_LOG("directory already there for topic {}", topic);
         }
+
         INFO_LOG("new topic created: {}", topic);
     }
     else
@@ -48,7 +55,76 @@ void Topics::create_topic(std::string const topic)
     }
 }
 
-void Topics::delete_topic(std::string const topic)
+// function to delete a topic.
+// removes the associated directory as well if second arg is true.
+void Topics::delete_topic(std::string const topic, bool const delete_directory)
 {
-    // TODO: delete topic from list as well its directory
+    if (exists(topic))
+    {
+        topics.erase(topic);
+
+        // deleting the entire topics directory
+        if (delete_directory)
+        {
+            fs::path path = (topics_directory + "/" + topic);
+
+            if (fs::exists(path))
+            {
+                fs::remove_all(path);
+            }
+        }
+    }
+    else
+    {
+        WARN_LOG("deleting non-existent topic {}", topic);
+    }
+}
+
+// increments the counter of messages for this topic by 1
+void Topics::increment_mssg_cnt(std::string const topic)
+{
+    if (exists(topic))
+    {
+        topics[topic]++;
+    }
+    else
+    {
+        WARN_LOG("incrementing non-existent topic {}", topic);
+    }
+}
+
+// decrements the counter of messages for this topic by 1
+void Topics::decrement_mssg_cnt(std::string const topic)
+{
+    if (exists(topic))
+    {
+        if (topics[topic] > 0)
+        {
+            topics[topic]--;
+        }
+        else
+        {
+            WARN_LOG("can't decrement zero cnt for topic {}", topic);
+        }
+    }
+    else
+    {
+        WARN_LOG("decrementing non-existent topic {}", topic);
+    }
+}
+
+// returns count of messages for this topic.
+// returns 0 even if topics DNE. Ideally check if topic
+// exists before callling this function.
+unsigned Topics::get_mssg_cnt(std::string const topic)
+{
+    if (exists(topic))
+    {
+        return topics[topic];
+    }
+    else
+    {
+        WARN_LOG("get_mssg_cnt called for non-existent topic {}", topic);
+        return 0;
+    }
 }
