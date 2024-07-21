9822efb4e362fed90ecd9747bc144fa65375b81f
shubham <>
2024-07-21 22:43:13 +0530

    * DEBUG_BUILD flag * warnings as errors * Crow, boost, main setup

--- src/logger.cpp
+++ src/logger.cpp
@@ -1,12 +1,16 @@
 #include "logger.h"
 #include "spdlog/common.h"
+#include "spdlog/spdlog.h"
 #define FILE_SIZE 5 * 1024 * 1024
 #define NUM_FILES 3
+#define FLUSH_INTERVAL 3
 
 static std::shared_ptr<spdlog::logger> create_logger()
 {
     std::shared_ptr<spdlog::logger> file_logger =
         spdlog::rotating_logger_mt<spdlog::async_factory>("logs", "logs/log.log", FILE_SIZE, NUM_FILES);
+
+    spdlog::flush_every(std::chrono::seconds(FLUSH_INTERVAL));
 
 #ifdef DEBUG_BUILD
     file_logger->set_level(spdlog::level::debug);
