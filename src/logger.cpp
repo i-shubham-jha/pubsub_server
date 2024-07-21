9d3f7775b3c34ed14346455e8a6d7509b2d99f1b
shubham <>
2024-07-21 15:19:59 +0530

    debug logs enabled thru cmake option

--- src/logger.cpp
+++ src/logger.cpp
@@ -1,6 +1,18 @@
 #include "logger.h"
+#include "spdlog/common.h"
 #define FILE_SIZE 5 * 1024 * 1024
 #define NUM_FILES 3
 
-std::shared_ptr<spdlog::logger> file_logger =
-    spdlog::rotating_logger_mt<spdlog::async_factory>("logs", "logs/log.log", FILE_SIZE, NUM_FILES);
+static std::shared_ptr<spdlog::logger> create_logger()
+{
+    std::shared_ptr<spdlog::logger> file_logger =
+        spdlog::rotating_logger_mt<spdlog::async_factory>("logs", "logs/log.log", FILE_SIZE, NUM_FILES);
+
+#ifdef DEBUG_BUILD
+    file_logger->set_level(spdlog::level::debug);
+#endif
+
+    return file_logger;
+}
+
+std::shared_ptr<spdlog::logger> file_logger = create_logger();
