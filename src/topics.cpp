91cb4868e9d5230cc09ccde06839cae5cddcdbca
shubham <>
2024-07-23 19:46:24 +0530

    small change

--- src/topics.cpp
+++ src/topics.cpp
@@ -38,7 +38,7 @@ void Topics::create_topic(std::string const topic)
     if (!exists(topic))
     {
         /* inserting into map */
-        topics[topic];
+        topics[topic] = 0;
 
         /* creating directory for this topic */
         fs::path path = (topics_directory + "/" + topic);
